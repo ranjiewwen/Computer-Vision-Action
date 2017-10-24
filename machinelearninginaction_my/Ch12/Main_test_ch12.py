15e538a6f4948e6d3b9f974f200170a84add4d6e
ranjiewwen <532685734@qq.com>
2017-10-24 21:29:48 +0800

    update

--- machinelearninginaction_my/Ch12/Main_test_ch12.py
+++ machinelearninginaction_my/Ch12/Main_test_ch12.py
@@ -1,2 +1,19 @@
 # coding:utf-8
 
+import fpGrowth
+
+rootNode=fpGrowth.treeNode('pyramid',9,None)
+
+rootNode.children['eye']=fpGrowth.treeNode('eye',13,None)
+
+rootNode.disp()
+
+rootNode.children['phoenix']=fpGrowth.treeNode('phoenix',3,None)
+
+
+simpDat=fpGrowth.loadSimpDat()
+initSet=fpGrowth.createInitSet(simpDat)
+print(initSet)
+
+myFPtree,myHeaderTab=fpGrowth.createTree(initSet,3)
+myFPtree.disp()
--- machinelearninginaction_my/Ch12/fpGrowth.py
+++ machinelearninginaction_my/Ch12/fpGrowth.py
@@ -116,42 +116,42 @@ def createInitSet(dataSet):
         retDict[frozenset(trans)] = 1
     return retDict
 
-import twitter
-from time import sleep
-import re
-
-def textParse(bigString):
-    urlsRemoved = re.sub('(http:[/][/]|www.)([a-z]|[A-Z]|[0-9]|[/.]|[~])*', '', bigString)    
-    listOfTokens = re.split(r'\W*', urlsRemoved)
-    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
-
-def getLotsOfTweets(searchStr):
-    CONSUMER_KEY = ''
-    CONSUMER_SECRET = ''
-    ACCESS_TOKEN_KEY = ''
-    ACCESS_TOKEN_SECRET = ''
-    api = twitter.Api(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET,
-                      access_token_key=ACCESS_TOKEN_KEY, 
-                      access_token_secret=ACCESS_TOKEN_SECRET)
-    #you can get 1500 results 15 pages * 100 per page
-    resultsPages = []
-    for i in range(1,15):
-        print "fetching page %d" % i
-        searchResults = api.GetSearch(searchStr, per_page=100, page=i)
-        resultsPages.append(searchResults)
-        sleep(6)
-    return resultsPages
-
-def mineTweets(tweetArr, minSup=5):
-    parsedList = []
-    for i in range(14):
-        for j in range(100):
-            parsedList.append(textParse(tweetArr[i][j].text))
-    initSet = createInitSet(parsedList)
-    myFPtree, myHeaderTab = createTree(initSet, minSup)
-    myFreqList = []
-    mineTree(myFPtree, myHeaderTab, minSup, set([]), myFreqList)
-    return myFreqList
+# import twitter
+# from time import sleep
+# import re
+#
+# def textParse(bigString):
+#     urlsRemoved = re.sub('(http:[/][/]|www.)([a-z]|[A-Z]|[0-9]|[/.]|[~])*', '', bigString)
+#     listOfTokens = re.split(r'\W*', urlsRemoved)
+#     return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+#
+# def getLotsOfTweets(searchStr):
+#     CONSUMER_KEY = ''
+#     CONSUMER_SECRET = ''
+#     ACCESS_TOKEN_KEY = ''
+#     ACCESS_TOKEN_SECRET = ''
+#     api = twitter.Api(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET,
+#                       access_token_key=ACCESS_TOKEN_KEY,
+#                       access_token_secret=ACCESS_TOKEN_SECRET)
+#     #you can get 1500 results 15 pages * 100 per page
+#     resultsPages = []
+#     for i in range(1,15):
+#         print "fetching page %d" % i
+#         searchResults = api.GetSearch(searchStr, per_page=100, page=i)
+#         resultsPages.append(searchResults)
+#         sleep(6)
+#     return resultsPages
+#
+# def mineTweets(tweetArr, minSup=5):
+#     parsedList = []
+#     for i in range(14):
+#         for j in range(100):
+#             parsedList.append(textParse(tweetArr[i][j].text))
+#     initSet = createInitSet(parsedList)
+#     myFPtree, myHeaderTab = createTree(initSet, minSup)
+#     myFreqList = []
+#     mineTree(myFPtree, myHeaderTab, minSup, set([]), myFreqList)
+#     return myFreqList
 
 #minSup = 3
 #simpDat = loadSimpDat()
